fe478906d7f4a895eec231c699a916191bf1002e
Emilio Moya <efm8972@nyu.edu>
2024-04-11 16:11:41 +0800

    Test commit

--- tempotalk/src/main/java/com/project/tempotalk/security/WebSecurityConfig.java
+++ tempotalk/src/main/java/com/project/tempotalk/security/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll().requestMatchers("/api/test/**")
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll().requestMatchers("/api/**")
                     .permitAll().anyRequest().authenticated());
 
         http.authenticationProvider(authenticationProvider());
