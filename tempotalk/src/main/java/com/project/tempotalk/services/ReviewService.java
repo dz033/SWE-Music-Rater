6c8ee7644764c0e9f3ea7b6ae6630c6c39273951
Emilio Moya <efm8972@nyu.edu>
2024-04-11 16:22:32 +0800

    Added an endpoint to get all reviews

--- tempotalk/src/main/java/com/project/tempotalk/services/ReviewService.java
+++ tempotalk/src/main/java/com/project/tempotalk/services/ReviewService.java
@@ -1,8 +1,19 @@
 package com.project.tempotalk.services;
 
+import com.project.tempotalk.models.Album;
+import com.project.tempotalk.models.Review;
+import com.project.tempotalk.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ReviewService {
+    @Autowired
+    ReviewRepository reviewRepository;
 
+    public List<Review> allReviews(){
+        return reviewRepository.findAll();
+    }
 }
