cbbf9a015a0a63c45a2be6c03bc1f6f02987d92b
Emilio Moya <54635784+emoya620@users.noreply.github.com>
2024-04-11 15:02:03 +0800

    Delete Music-Rater directory

--- Music-Rater/src/main/java/com/musicrater/MusicRater/models/ERole.java
+++
@@ -1,6 +0,0 @@
-package com.musicrater.MusicRater.models;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- Music-Rater/src/main/java/com/musicrater/MusicRater/services/ArtistService.java
+++
@@ -1,8 +0,0 @@
-package com.musicrater.MusicRater.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ArtistService {
-
-}
