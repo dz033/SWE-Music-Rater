b0decabf5bdb825b01adfb6e411bfe665f88ba44
Emilio Moya <efm8972@nyu.edu>
2024-04-18 15:51:48 +0800

    Created a test "createReview" function

--- tempotalk/src/main/java/com/project/tempotalk/controllers/ReviewController.java
+++ tempotalk/src/main/java/com/project/tempotalk/controllers/ReviewController.java
@@ -5,7 +5,9 @@ import com.project.tempotalk.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class ReviewController {
     @GetMapping
     public ResponseEntity<List<Review>> getAllReviews(){
         return new ResponseEntity<List<Review>>(reviewService.allReviews(), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<?> createReview(){
+        return new ResponseEntity<String>("Review Created", HttpStatus.OK);
     }
 }
