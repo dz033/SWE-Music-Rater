7374e2935748061ea7be49298b70cd95c3aa58f3
Emilio Moya <efm8972@nyu.edu>
2024-04-22 22:23:36 +0800

    Added the "getAllArtists" and "getArtistsByName" functions

--- tempotalk/src/main/java/com/project/tempotalk/models/Artist.java
+++ tempotalk/src/main/java/com/project/tempotalk/models/Artist.java
@@ -1,12 +1,10 @@
 package com.project.tempotalk.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.ArrayList;
 import java.util.List;
