5ca7b71753bd74caabe68ff16eecd6796aa0296c
karolina-avv <118016776+karolina-avv@users.noreply.github.com>
2024-04-25 15:24:15 +0800

    color

--- music-rater-frontend/src/pages/new.js
+++ music-rater-frontend/src/pages/new.js
@@ -3,8 +3,13 @@ import './new.css';
 export default function NewPage() {
     return (
         <div className="new-page"> 
-            <h1>New Releases</h1>
-            <div>
+        <div className="left-column">
+                <h1>New Releases</h1>
+                <p>Put albums whose release data less 1 month</p>
+            </div>
+            <div className="right-column">
+                <h2>Upcoming</h2>
+                <p>Stay tuned for these upcoming albums!</p>
             </div>
         </div>
     );
