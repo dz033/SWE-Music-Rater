b1bc85cfa5df1d3173263af4cd07a461c23df0ea
Emilio Moya <efm8972@nyu.edu>
2024-04-25 23:59:50 +0800

    Created an EditReviewRequest class to handle requests to edit a review

---
+++ tempotalk/src/main/java/com/project/tempotalk/payload/request/EditReviewRequest.java
@@ -0,0 +1,13 @@
+package com.project.tempotalk.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EditReviewRequest {
+    private String body;
+    private int rating;
+    private String reviewId;
+    private String musicId;
+}
