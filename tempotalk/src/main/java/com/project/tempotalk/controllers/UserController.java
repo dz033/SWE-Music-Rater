83b828ca6fc843dea1274cf4fe658323c1f72cf6
Emilio Moya <efm8972@nyu.edu>
2024-04-27 16:26:59 +0800

    Added the @CrossOrigin annotation so that the frontend can interact with the different controllers

--- tempotalk/src/main/java/com/project/tempotalk/controllers/UserController.java
+++ tempotalk/src/main/java/com/project/tempotalk/controllers/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
