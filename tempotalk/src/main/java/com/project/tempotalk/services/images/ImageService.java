fb04960cf292515e0737368229bc8a8aa3e4daa6
Emilio Moya <efm8972@nyu.edu>
2024-04-27 17:31:18 +0800

    Added "uploadSongImage", "uploadProfileImage", and  "uploadArtistImage" functions

--- tempotalk/src/main/java/com/project/tempotalk/services/images/ImageService.java
+++ tempotalk/src/main/java/com/project/tempotalk/services/images/ImageService.java
@@ -1,8 +1,12 @@
 package com.project.tempotalk.services.images;
 
+import com.project.tempotalk.payload.request.ImageUploadRequest;
 import com.project.tempotalk.payload.response.ImageUploadResponse;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageService {
-    ImageUploadResponse uploadImage(MultipartFile multipartFile);
+    ImageUploadResponse uploadAlbumImage(ImageUploadRequest imageUploadRequest);
+    ImageUploadResponse uploadSongImage(ImageUploadRequest imageUploadRequest);
+    ImageUploadResponse uploadProfileImage(ImageUploadRequest imageUploadRequest);
+    ImageUploadResponse uploadArtistImage(ImageUploadRequest imageUploadRequest);
 }
