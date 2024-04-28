71a757f175043dd08346a7f1cea8d1e04fe0b855
Emilio Moya <efm8972@nyu.edu>
2024-04-28 22:30:44 +0800

    Added the "getRandomAlbums" function, which randomly selects albums from our database

--- tempotalk/src/main/java/com/project/tempotalk/services/AlbumService.java
+++ tempotalk/src/main/java/com/project/tempotalk/services/AlbumService.java
@@ -5,8 +5,13 @@ import com.project.tempotalk.payload.request.AlbumRequest;
 import com.project.tempotalk.payload.response.MessageResponse;
 import com.project.tempotalk.repositories.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.aggregation.SampleOperation;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +19,9 @@ import java.util.Optional;
 public class AlbumService {
     @Autowired
     AlbumRepository albumRepository;
+
+    @Autowired
+    MongoTemplate mongoTemplate;
 
     // Return a list of all albums in albumRepository
     public List<Album> allAlbums(){
@@ -23,6 +31,18 @@ public class AlbumService {
     // Return a list of all albums that exist by a title, if there are any
     public Optional<List<Album>> albumsByTitle(String title){
         return albumRepository.findAlbumByTitle(title);
+    }
+
+    // Randomly select albums from the database
+    public List<Album> getRandomAlbums(int numAlbums){
+        List<Album> randomAlbums;
+
+        SampleOperation sampleStage = Aggregation.sample(numAlbums);
+        Aggregation aggregation = Aggregation.newAggregation(sampleStage);
+        AggregationResults<Album> output = mongoTemplate.aggregate(aggregation, "albums", Album.class);
+        randomAlbums = output.getMappedResults();
+
+        return  randomAlbums;
     }
 
     // Create a new album object and store it in our "albums" collection
