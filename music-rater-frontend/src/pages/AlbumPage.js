0fd4294f7bd01a004842294a794871d2e9e976a6
karolina-avv <118016776+karolina-avv@users.noreply.github.com>
2024-04-29 14:06:31 +0800

    working on getting individual album pages
    problem with the API endpoint to correct later

--- music-rater-frontend/src/pages/AlbumPage.js
+++ music-rater-frontend/src/pages/AlbumPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import Album from '../components/Album';
 
-export default function AlbumPage() {
-    return (
-        <div className="album-page">
-            <Album />
-        </div>
-    );
+function AlbumPage() {
+    const { id } = useParams(); // Get the id parameter from the URL
+    console.log("ID from URL:", id); 
+return (
+    <div>
+      <h2>Album Page</h2>
+      <Album id={id} /> {/* Pass the id to the Album component */}
+    </div>
+  );
 }
+
+export default AlbumPage;
