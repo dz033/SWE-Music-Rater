5cb87a69f3c3a882dd09c328be3a8cb6f3d46e1e
dz033 <121326266+dz033@users.noreply.github.com>
2024-04-29 23:50:47 +0800

    album page changes

--- music-rater-frontend/src/components/Review.js
+++ music-rater-frontend/src/components/Review.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../pages/home.css'
 
 const API_DIR = "http://localhost:8080/";
 
@@ -21,14 +22,14 @@ function Review({music_id}) {
 
     return (
         <div className="review">
-          <h1>Reviews</h1>
+          <h1>Reviews:</h1>
           <div className="review-list">
             {reviews.map(review => (
               <div key={review.id} className="review-item">
                 <p>Score: {review.score}</p>
                 <p>{review.body}</p>
                 <p>Date: {review.creationDate.slice(0, 10)}</p>
-                
+
               </div>
             ))}
           </div>
