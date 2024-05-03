b19910a3463bd912f4eeb2282eedc484c5f6c0a2
Emilio Moya <efm8972@nyu.edu>
2024-05-03 22:45:39 +0800

    Created PlaylistController.java

---
+++ tempotalk/src/main/java/com/project/tempotalk/controllers/PlaylistController.java
@@ -0,0 +1,12 @@
+package com.project.tempotalk.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/playlists")
+public class PlaylistController {
+
+}
