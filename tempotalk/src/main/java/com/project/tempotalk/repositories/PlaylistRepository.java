9e17f7ef543627dcfb5b729edc7a1bcdf57211c8
Emilio Moya <efm8972@nyu.edu>
2024-05-04 01:38:55 +0800

    Added "findPlaylistsByOwnerId" function

--- tempotalk/src/main/java/com/project/tempotalk/repositories/PlaylistRepository.java
+++ tempotalk/src/main/java/com/project/tempotalk/repositories/PlaylistRepository.java
@@ -4,7 +4,10 @@ import com.project.tempotalk.models.Playlist;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PlaylistRepository extends MongoRepository<Playlist, String> {
-
+    Optional<List<Playlist>> findPlaylistsByOwnerId(String ownerId);
 }
