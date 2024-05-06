08e8bc8c53df00e9c4a8f5a26c9922efc5575dcd
Emilio Moya <efm8972@nyu.edu>
2024-05-06 18:28:50 +0800

    Added attributes to "SongRequest"

--- tempotalk/src/main/java/com/project/tempotalk/payload/request/SongRequest.java
+++ tempotalk/src/main/java/com/project/tempotalk/payload/request/SongRequest.java
@@ -3,9 +3,13 @@ package com.project.tempotalk.payload.request;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 public class SongRequest {
     private String title;
+    private String artist;
     private String releaseDate;
+    private List<String> genres;
 }
