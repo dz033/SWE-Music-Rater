d69dac58241e44fa72552a22262ba060df30ff93
Emilio Moya <efm8972@nyu.edu>
2024-05-10 15:18:00 +0800

    Added more unit tests

--- tempotalk/src/test/java/com/project/tempotalk/unittests/AuthServiceTests.java
+++ tempotalk/src/test/java/com/project/tempotalk/unittests/AuthServiceTests.java
@@ -1,25 +1,29 @@
 package com.project.tempotalk.unittests;
 
+import com.project.tempotalk.models.ERole;
+import com.project.tempotalk.models.Role;
 import com.project.tempotalk.models.User;
 import com.project.tempotalk.payload.request.SignupRequest;
-import com.project.tempotalk.payload.response.MessageResponse;
+import com.project.tempotalk.payload.response.AuthResponse;
+import com.project.tempotalk.repositories.RoleRepository;
 import com.project.tempotalk.repositories.UserRepository;
 import com.project.tempotalk.services.AuthService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.Before;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static com.project.tempotalk.models.ERole.ROLE_ADMIN;
+import static com.project.tempotalk.models.ERole.ROLE_USER;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 //David
@@ -29,56 +33,95 @@ public class AuthServiceTests{
     @Mock
     private UserRepository userRepository;
 
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Mock
+    private PasswordEncoder encoder;
+
     @InjectMocks
     private AuthService authService;
 
+    private User user;
+    private User adminUser;
+    private Optional<Role> userRole;
+    private Optional<Role> adminRole;
+
+    @BeforeEach
+    public void init(){
+        userRole = Optional.of(new Role(ROLE_USER));
+        adminRole = Optional.of(new Role(ROLE_ADMIN));
+        Set<Role> roles = new HashSet<>();
+        roles.add(new Role(ROLE_USER));
+
+        user = new User("test", "test@gmail.com", "password");
+        user.setRoles(roles);
+
+        roles.clear();
+
+        roles.add(new Role(ROLE_ADMIN));
+
+        adminUser = new User("admin", "admin@gmail.com", "password");
+        adminUser.setRoles(roles);
+    }
+
     @Test
-    void duplicateUsernameError(){
+    void AuthService_RegisterUser_DuplicateUsernameError(){
         Set<String> roles = new HashSet<>();
         roles.add("user");
         SignupRequest request = new SignupRequest("bobjoe", "bobjoe@gmail.com", roles, "password");
         when(userRepository.existsByUsername(Mockito.any(String.class))).thenReturn(true);
-        MessageResponse response = authService.registerUser(request);
+        AuthResponse response = authService.registerUser(request);
         assertEquals("Error: Username is already taken!", response.getMessage());
     }
 
     @Test
-    void duplicateEmailError(){
+    void AuthService_RegisterUser_DuplicateEmailError(){
         Set<String> roles = new HashSet<>();
         roles.add("user");
         SignupRequest request = new SignupRequest("bobjoe", "bobjoe@gmail.com", roles, "password");
         when(userRepository.existsByEmail(Mockito.any(String.class))).thenReturn(true);
-        MessageResponse response = authService.registerUser(request);
+        AuthResponse response = authService.registerUser(request);
         assertEquals("Error: Email is already in use!", response.getMessage());
     }
     
-//    @Test
-//    void rolesSpecifiedUser(){
-//        Set<String> roles = new HashSet<>();
-//        roles.add("user");
-//        SignupRequest request = new SignupRequest("bobjoe", "bobjoe@gmail.com", roles, "password");
-//        when(userRepository.save(Mockito.any(User.class))).thenReturn(new User());
-//        MessageResponse response = authService.registerUser(request);
-//        assertEquals("User registered successfully!", response.getMessage());
-//    }
-//
-//    @Test
-//    void rolesSpecifiedAdmin(){
-//        Set<String> roles = new HashSet<>();
-//        roles.add("admin");
-//        SignupRequest request = new SignupRequest("bobjoe", "bobjoe@gmail.com", roles, "password");
-//        when(userRepository.save(Mockito.any(User.class))).thenReturn(new User());
-//        Mockito.when(userService.passwordEncoder()).thenReturn(new BCryptPasswordEncoder());
-//        MessageResponse response = authService.registerUser(request);
-//        assertEquals("User registered successfully!", response.getMessage());
-//    }
-//
-//    @Test
-//    void rolesUnspecified(){
-//        Set<String> roles = new HashSet<>();
-//        SignupRequest request = new SignupRequest("bobjoe", "bobjoe@gmail.com", roles, "password");
-//        when(userRepository.save(Mockito.any(User.class))).thenReturn(new User());
-//        MessageResponse response = authService.registerUser(request);
-//        assertEquals("User registered successfully!", response.getMessage());
-//    }
+    @Test
+    void AuthService_RegisterUser_RolesSpecifiedUser(){
+        Set<String> roles = new HashSet<>();
+        roles.add("user");
+        SignupRequest request = new SignupRequest("test", "test@gmail.com", roles, "password");
+        when(roleRepository.findByName(Mockito.any(ERole.class))).thenReturn(userRole);
+        when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
+        when(encoder.encode(Mockito.anyString())).thenReturn("password");
+        AuthResponse response = authService.registerUser(request);
+        assertEquals("User registered successfully!", response.getMessage());
+        assertTrue(response.getUser().getRoles().contains(new Role(ROLE_USER)));
+        assertNotNull(response.getUser());
+    }
+
+    @Test
+    void AuthService_RegisterUser_RolesSpecifiedAdmin(){
+        Set<String> roles = new HashSet<>();
+        roles.add("admin");
+        SignupRequest request = new SignupRequest("admin", "admin@gmail.com", roles, "password");
+        when(roleRepository.findByName(Mockito.any(ERole.class))).thenReturn(adminRole);
+        when(userRepository.save(Mockito.any(User.class))).thenReturn(adminUser);
+        when(encoder.encode(Mockito.anyString())).thenReturn("password");
+        AuthResponse response = authService.registerUser(request);
+        assertEquals("User registered successfully!", response.getMessage());
+        assertTrue(response.getUser().getRoles().contains(new Role(ROLE_ADMIN)));
+        assertNotNull(response.getUser());
+    }
+
+    @Test
+    void AuthService_RegisterUser_RolesUnspecified(){
+        SignupRequest request = new SignupRequest("test", "test@gmail.com", null, "password");
+        when(roleRepository.findByName(Mockito.any(ERole.class))).thenReturn(userRole);
+        when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
+        when(encoder.encode(Mockito.anyString())).thenReturn("password");
+        AuthResponse response = authService.registerUser(request);
+        assertEquals("User registered successfully!", response.getMessage());
+        assertTrue(response.getUser().getRoles().contains(new Role(ROLE_USER)));
+        assertNotNull(response.getUser());
+    }
 }
