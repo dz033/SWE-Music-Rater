94c168965dc817d14ccfa9f4c66e43f34aa15893
Emilio Moya <efm8972@nyu.edu>
2024-05-11 20:25:55 +0800

    Added unit tests for the "ReviewService" class

--- tempotalk/src/test/java/com/project/tempotalk/unittests/ReviewServiceTests.java
+++ tempotalk/src/test/java/com/project/tempotalk/unittests/ReviewServiceTests.java
@@ -2,7 +2,9 @@ package com.project.tempotalk.unittests;
 
 import com.project.tempotalk.models.Album;
 import com.project.tempotalk.models.Review;
+import com.project.tempotalk.models.Song;
 import com.project.tempotalk.models.User;
+import com.project.tempotalk.payload.request.EditReviewRequest;
 import com.project.tempotalk.payload.request.ReviewRequest;
 import com.project.tempotalk.payload.response.ReviewResponse;
 import com.project.tempotalk.repositories.AlbumRepository;
@@ -16,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -51,6 +54,7 @@ public class ReviewServiceTests {
     ReviewService reviewService;
 
     private Album album;
+    private Song song;
     private User user;
     private Review review;
     private List<String> userReviews;
@@ -58,6 +62,7 @@ public class ReviewServiceTests {
     @BeforeEach
     public void init(){
         album = new Album("Wall of Eyes", "The Smile", "12-01-01", new ArrayList<>(Arrays.asList("Alternative Rock")));
+        song = new Song("title", "artist", "releaseDate", new ArrayList<>(Arrays.asList("Alternative Rock")));
         user = new User("user", "temp@gmail.com", "password");
         review = new Review("body", 100, "userId", "musicId");
         userReviews = new ArrayList<>(Arrays.asList("reviewId1", "reviewId2"));
@@ -109,5 +114,180 @@ public class ReviewServiceTests {
         ReviewResponse response = reviewService.createReview(reviewRequest);
         assertThat(response.getReview()).isNotNull();
         assertThat(response.getMessage()).isEqualTo("Review created successfully!");
+    }
+
+    @Test
+    public void ReviewService_UpdateReview_ReviewNotFound(){
+        EditReviewRequest request = new EditReviewRequest("body", 100, "reviewId", "musicId");
+        when(reviewRepository.existsById(Mockito.anyString())).thenReturn(false);
+        ReviewResponse response = reviewService.updateReview(request);
+        assertThat(response.getMessage()).isEqualTo("Error: Review was not found");
+        assertThat(response.getReview()).isNull();
+        assertThat(response.getUser()).isNull();
+        assertThat(response.getAlbum()).isNull();
+        assertThat(response.getSong()).isNull();
+    }
+
+    @Test
+    public void ReviewService_UpdateReview_ReviewNotFound2(){
+        EditReviewRequest request = new EditReviewRequest("body", 100, "reviewId", "musicId");
+        when(reviewRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(albumRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(reviewRepository.findById(request.getReviewId())).thenReturn(Optional.empty());
+        ReviewResponse response = reviewService.updateReview(request);
+        assertThat(response.getMessage()).isEqualTo("Error: Review was not found");
+        assertThat(response.getReview()).isNull();
+        assertThat(response.getUser()).isNull();
+        assertThat(response.getAlbum()).isNull();
+        assertThat(response.getSong()).isNull();
+    }
+
+    @Test
+    public void ReviewService_UpdateReview_MusicNotFound(){
+        EditReviewRequest request = new EditReviewRequest("body", 100, "reviewId", "musicId");
+        when(reviewRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(albumRepository.existsById(Mockito.anyString())).thenReturn(false);
+        when(songRepository.existsById(Mockito.anyString())).thenReturn(false);
+        ReviewResponse response = reviewService.updateReview(request);
+        assertThat(response.getMessage()).isEqualTo("Error: Album or song was not found");
+        assertThat(response.getReview()).isNull();
+        assertThat(response.getUser()).isNull();
+        assertThat(response.getAlbum()).isNull();
+        assertThat(response.getSong()).isNull();
+    }
+
+    @Test
+    public void ReviewService_UpdateReview_MusicNotFound2(){
+        EditReviewRequest request = new EditReviewRequest("body", 100, "reviewId", "musicId");
+        when(reviewRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(albumRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(reviewRepository.findById(request.getReviewId())).thenReturn(Optional.of(review));
+        when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        when(reviewRepository.save(Mockito.any(Review.class))).thenReturn(review);
+        ReviewResponse response = reviewService.updateReview(request);
+        assertThat(response.getMessage()).isEqualTo("Error: Review was not associated with an album or song");
+        assertThat(response.getReview()).isNull();
+        assertThat(response.getUser()).isNull();
+        assertThat(response.getAlbum()).isNull();
+        assertThat(response.getSong()).isNull();
+    }
+
+    @Test
+    public void ReviewService_UpdateReview_UserNotFound(){
+        EditReviewRequest request = new EditReviewRequest("body", 100, "reviewId", "musicId");
+        when(reviewRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(albumRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(reviewRepository.findById(request.getReviewId())).thenReturn(Optional.of(review));
+        when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.empty());
+        ReviewResponse response = reviewService.updateReview(request);
+        assertThat(response.getMessage()).isEqualTo("Error: User not found");
+        assertThat(response.getReview()).isNull();
+        assertThat(response.getUser()).isNull();
+        assertThat(response.getAlbum()).isNull();
+        assertThat(response.getSong()).isNull();
+    }
+
+    @Test
+    public void ReviewService_UpdateReview_Success(){
+        EditReviewRequest request = new EditReviewRequest("body", 100, "reviewId", "musicId");
+        when(reviewRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(albumRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(reviewRepository.findById(request.getReviewId())).thenReturn(Optional.of(review));
+        when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        when(reviewRepository.save(Mockito.any(Review.class))).thenReturn(review);
+        when(albumRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(albumRepository.findById(Mockito.anyString())).thenReturn(Optional.of(album));
+        when(albumRepository.save(Mockito.any(Album.class))).thenReturn(album);
+        ReviewResponse response = reviewService.updateReview(request);
+        assertThat(response.getMessage()).isEqualTo("Review updated successfully!");
+        assertThat(response.getReview()).isNotNull();
+        assertThat(response.getUser()).isNotNull();
+        assertThat(response.getAlbum()).isNotNull();
+        assertThat(response.getSong()).isNull();
+    }
+
+    @Test
+    public void ReviewService_UpdateReview_Success2(){
+        EditReviewRequest request = new EditReviewRequest("body", 100, "reviewId", "musicId");
+        when(reviewRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(songRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(reviewRepository.findById(request.getReviewId())).thenReturn(Optional.of(review));
+        when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        when(reviewRepository.save(Mockito.any(Review.class))).thenReturn(review);
+        when(songRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(songRepository.findById(Mockito.anyString())).thenReturn(Optional.of(song));
+        when(songRepository.save(Mockito.any(Song.class))).thenReturn(song);
+        ReviewResponse response = reviewService.updateReview(request);
+        assertThat(response.getMessage()).isEqualTo("Review updated successfully!");
+        assertThat(response.getReview()).isNotNull();
+        assertThat(response.getUser()).isNotNull();
+        assertThat(response.getSong()).isNotNull();
+        assertThat(response.getAlbum()).isNull();
+    }
+
+    @Test
+    public void ReviewService_DeleteReview_ReviewNotFound(){
+        when(reviewRepository.findById(Mockito.anyString())).thenReturn(Optional.empty());
+        ReviewResponse response = reviewService.deleteReview("reviewId");
+        assertThat(response.getMessage()).isEqualTo("Error: Review was not found");
+        assertThat(response.getReview()).isNull();
+        assertThat(response.getUser()).isNull();
+        assertThat(response.getAlbum()).isNull();
+        assertThat(response.getSong()).isNull();
+    }
+
+    @Test
+    public void ReviewService_DeleteReview_UserNotFound(){
+        when(reviewRepository.findById(Mockito.anyString())).thenReturn(Optional.of(review));
+        when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.empty());
+        ReviewResponse response = reviewService.deleteReview("reviewId");
+        assertThat(response.getMessage()).isEqualTo("Error: Review was not associated with a user");
+        assertThat(response.getReview()).isNull();
+        assertThat(response.getUser()).isNull();
+        assertThat(response.getAlbum()).isNull();
+        assertThat(response.getSong()).isNull();
+    }
+
+    @Test
+    public void ReviewService_DeleteReview_MusicNotFound(){
+        when(reviewRepository.findById(Mockito.anyString())).thenReturn(Optional.of(review));
+        when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
+        ReviewResponse response = reviewService.deleteReview("reviewId");
+        assertThat(response.getMessage()).isEqualTo("Error: Review was not associated with an album or song");
+        assertThat(response.getReview()).isNull();
+        assertThat(response.getUser()).isNull();
+        assertThat(response.getAlbum()).isNull();
+        assertThat(response.getSong()).isNull();
+    }
+
+    @Test
+    public void ReviewService_DeleteReview_Success(){
+        when(reviewRepository.findById(Mockito.anyString())).thenReturn(Optional.of(review));
+        when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
+        when(albumRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(albumRepository.findById(Mockito.anyString())).thenReturn(Optional.of(album));
+        ReviewResponse response = reviewService.deleteReview("reviewId");
+        assertThat(response.getMessage()).isEqualTo("Review deleted successfully!");
+        assertThat(response.getReview()).isNotNull();
+        assertThat(response.getUser()).isNotNull();
+        assertThat(response.getAlbum()).isNotNull();
+        assertThat(response.getSong()).isNull();
+    }
+
+    @Test
+    public void ReviewService_DeleteReview_Success2(){
+        when(reviewRepository.findById(Mockito.anyString())).thenReturn(Optional.of(review));
+        when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        when(userRepository.save(Mockito.any(User.class))).thenReturn(user);
+        when(songRepository.existsById(Mockito.anyString())).thenReturn(true);
+        when(songRepository.findById(Mockito.anyString())).thenReturn(Optional.of(song));
+        ReviewResponse response = reviewService.deleteReview("reviewId");
+        assertThat(response.getMessage()).isEqualTo("Review deleted successfully!");
+        assertThat(response.getReview()).isNotNull();
+        assertThat(response.getUser()).isNotNull();
+        assertThat(response.getSong()).isNotNull();
+        assertThat(response.getAlbum()).isNull();
     }
 }
