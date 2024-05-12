29295f5fa2e54195e265f8e8fb429d0e2cbebb2f
Emilio Moya <efm8972@nyu.edu>
2024-05-12 20:43:04 +0800

    Added @EnableAutoConfiguration

--- tempotalk/src/main/java/com/project/tempotalk/TempotalkApplication.java
+++ tempotalk/src/main/java/com/project/tempotalk/TempotalkApplication.java
@@ -1,9 +1,11 @@
 package com.project.tempotalk;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class TempotalkApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TempotalkApplication.class, args);
